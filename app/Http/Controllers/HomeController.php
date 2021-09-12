32bbfd7295214ef00607e4db6d85ee70af679526
Mohammad Arman <arman.ctgbd1394@gmail.com>
2021-09-12 18:25:56 +0600

    Dashboard Updated

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,45 +15,26 @@ class HomeController extends BaseController
         if (permission('dashboard-access')){
 
             $this->setPageData('Dashboard','Dashboard','fas fa-technometer');
-            // $start_date = date('Y-m-d');
-            // $end_date   = date('Y-m-d');
-            // $sale = DB::table('sales')
-            //         ->whereDate('sale_date','>=',$start_date)
-            //         ->whereDate('sale_date','<=',$end_date)
-            //         ->sum('grand_total');
-            // $purchase = DB::table('purchases')->whereDate('purchase_date','>=',$start_date)
-            //             ->whereDate('purchase_date','<=',$end_date)
-            //             ->sum('grand_total');
-            // $income = DB::table('sales')
-            //             ->whereDate('sale_date','>=',$start_date)
-            //             ->whereDate('sale_date','<=',$end_date)
-            //             ->sum('paid_amount');
-            // $expense = DB::table('expenses')
-            // ->whereDate('date','>=',$start_date)
-            // ->whereDate('date','<=',$end_date)
-            // ->sum('amount');
+            //Yearly Report
+            $start = strtotime(date('Y').'-01-01');
+            $end = strtotime(date('Y').'-12-31');
 
+            $yearly_sale_amount = [];
+            $yearly_purchase_amount = [];
+            while ($start < $end) {
+                $start_date  = date('Y').'-'.date('m',$start).'-01';
+                $end_date  = date('Y').'-'.date('m',$start).'-31';
 
-             //Yearly Report
-             $start = strtotime(date('Y').'-01-01');
-             $end = strtotime(date('Y').'-12-31');
- 
-             $yearly_sale_amount = [];
-             $yearly_purchase_amount = [];
-             while ($start < $end) {
-                 $start_date  = date('Y').'-'.date('m',$start).'-01';
-                 $end_date  = date('Y').'-'.date('m',$start).'-31';
- 
-                 $sale_amount = DB::table('sales')->whereDate('sale_date','>=',$start_date)
-                 ->whereDate('sale_date','<=',$end_date)->sum('grand_total');
- 
-                 $purchase_amount = DB::table('purchases')->whereDate('purchase_date','>=',$start_date)
-                 ->whereDate('purchase_date','<=',$end_date)->sum('grand_total');
- 
-                 $yearly_sale_amount[] = number_format($sale_amount,2,'.','');
-                 $yearly_purchase_amount[] = number_format($purchase_amount,2,'.','');
-                 $start = strtotime('+1 month',$start);
-             }
+                $sale_amount = DB::table('sales')->whereDate('sale_date','>=',$start_date)
+                ->whereDate('sale_date','<=',$end_date)->sum('grand_total');
+
+                $purchase_amount = DB::table('purchases')->whereDate('purchase_date','>=',$start_date)
+                ->whereDate('purchase_date','<=',$end_date)->sum('grand_total');
+
+                $yearly_sale_amount[] = number_format($sale_amount,2,'.','');
+                $yearly_purchase_amount[] = number_format($purchase_amount,2,'.','');
+                $start = strtotime('+1 month',$start);
+            }
             return view('home',compact('yearly_sale_amount','yearly_purchase_amount'));
         }else{
             return redirect('unauthorized')->with(['status'=>'error','message'=>'Unauthorized Access Blocked']);
